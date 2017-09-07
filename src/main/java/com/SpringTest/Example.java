428a48da41db6a43fce29131f44cf6c07e7f33e4
keng <kenghong.chai@tu-dortmund.de>
2017-09-07 11:54:02 +0200

    added livereload support

--- src/main/java/com/SpringTest/Example.java
+++ src/main/java/com/SpringTest/Example.java
@@ -13,14 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class Example {
 
 
-
     @RequestMapping("/")
     String home()
     {
         return "Hello World";
     }
+
     public static void main (String[] args) throws Exception
     {
+        
         SpringApplication.run(Example.class,args);
 
     }
